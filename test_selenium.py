1740f24f9ac7a98efb2deb2c23281367d44768d7
jirufeng24 <37262950+jirufeng24@users.noreply.github.com>
2023-01-09 13:39:29 +0800

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,4 +1,4 @@
-#coding= utf-8
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from time import sleep
 
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 print("begin run the selenium scripts")
 driver.get("http://www.baidu.com") 
 
-driver.find_element_by_name("wd").send_keys("第五博客") 
+driver.find_element_by_name("wd").send_keys("Github") 
 driver.find_element_by_id("su").click()
 sleep(1)
 ele = driver.find_element_by_xpath('//*[@id="1"]/div/div[1]/h3/a').text
