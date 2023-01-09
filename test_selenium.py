7eee941c7f026ad98eaa5d576d2eeb4c2f835ab4
jirufeng24 <37262950+jirufeng24@users.noreply.github.com>
2023-01-09 13:16:15 +0800

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -8,7 +8,7 @@ driver.get("http://www.baidu.com")
 driver.find_element_by_name("wd").send_keys("第五博客") 
 driver.find_element_by_id("su").click()
 sleep(1)
-ele = driver.find_element_by_xpath('//*[@id="1"]/div/div[1]/h3/a').text()
+ele = driver.find_element_by_xpath('//*[@id="1"]/div/div[1]/h3/a').text
 print("text=",ele)
 
 driver.quit() 
