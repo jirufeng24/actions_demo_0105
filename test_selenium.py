058d63f64f20649986042783913af7891c4b7077
jirufeng24 <37262950+jirufeng24@users.noreply.github.com>
2023-01-09 13:13:43 +0800

    Update test_selenium.py

--- test_selenium.py
+++ test_selenium.py
@@ -5,10 +5,11 @@ driver = webdriver.Chrome()
 print("begin run the selenium scripts")
 driver.get("http://www.baidu.com") 
 
-# driver.find_element_by_id("kw").send_keys("第五博客") 
 driver.find_element_by_name("wd").send_keys("第五博客") 
 driver.find_element_by_id("su").click()
-sleep(3)
+sleep(1)
+ele = driver.find_element_by_xpath('//*[@id="1"]/div/div[1]/h3/a').text()
+print("text=",ele)
 
 driver.quit() 
 print("run over!!")
